cdbb408e70bf3c5b63067bc3c45691c6d8b42a29
ad988x <30480661+ad988x@users.noreply.github.com>
2018-01-07 15:23:34 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -117,7 +117,7 @@ function restaurantData(data) {
     });
 
     markers.push(marker);
-
+}
     // Create an onclick event to open the large infowindow at each marker.
     marker.addListener('click', function() {
       populateInfoWindow(this, largeInfowindow);
@@ -135,7 +135,7 @@ function restaurantData(data) {
     marker.addListener('mouseout', function() {
       this.setIcon(defaultIcon);
     });
-}
+
 
 function makeMarkerIcon(markerColor) {
   var markerImage = new google.maps.MarkerImage(
